b5bc93af44df1dff25b2e8d31848e5a859452bc5
yakir <yakir5814@gmail.com>
2026-05-16 17:40:30 +0300

    Update myLibrary.groovy

--- myLibrary.groovy
+++ myLibrary.groovy
@@ -1,8 +1,7 @@
 // vars/myLibrary.groovy
 def cleanup() {
     echo "--- [Cleanup] Cleaning up Jenkins workspace and removing temp files ---"
-    cleanWs() // פקודת ג'נקינס מובנית לניקוי התיקייה
-}
+    cleanWs() 
 
 def sendStatusEmail(String status) {
     echo "--- [Notification] Sending email notification with status: ${status} ---"
