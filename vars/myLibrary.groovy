20861ebc5e6fba996cd2b79f2bad785a17eb1c43
yakir <yakir5814@gmail.com>
2026-05-16 16:34:18 +0300

    Add pipeline stages for build, deploy, and cleanup

--- vars/myLibrary.groovy
+++ vars/myLibrary.groovy
@@ -1,1 +1,34 @@
+@Library('my-shared-library01') _
 
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                script {
+                    // קריאה לפונקציית הבנייה מהספרייה שלך
+                    myLibrary.buildApp()
+                }
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                script {
+                    // קריאה לפונקציית הפריסה
+                    myLibrary.deployApp('main')
+                }
+            }
+        }
+    }
+
+    post {
+        always {
+            script {
+                // קריאה לפונקציית הניקוי
+                myLibrary.cleanup()
+            }
+        }
+    }
+}
